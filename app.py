3a9fb536c776268c06c2b90791f41d46eaed01ea
FitzDS <146594341+FitzDS@users.noreply.github.com>
2024-12-08 23:12:46 -0800

    Update app.py

--- app.py
+++ app.py
@@ -223,7 +223,9 @@ if st.button("Search"):
 
             folium.Marker(
                 location=[row["latitude"], row["longitude"]],
-                popup=f"<b>{row['name']}</b><br>{TRANSLATIONS[language_code]['search_location']}: {row['address']}<br>Open Now: {row['open_now']}<br>Rating: {row['rating']} ({row['user_ratings_total']} reviews)",
+                popup=f"""<b>{row['name']}</b><br> {t('search_by_location')}: {row['address']}<br> Open Now: {row['open_now']}<br> Rating: {row['rating']} ({row['user_ratings_total']} reviews)<br>
+                <a href="https://www.google.com/maps/dir/?api=1&destination={row['latitude']},{row['longitude']}" target="_blank">Get Directions</a>
+                """, reviews)",
                 icon=folium.Icon(color=color)
             ).add_to(m)
 
